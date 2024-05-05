305143c3e86e9b131cec1b31b4943a3ed5e98475
Arailym <asermagambet@mail.ru>
2024-05-06 01:20:49 +0500

    add clear

--- START-HW15.playground/Contents.swift
+++ START-HW15.playground/Contents.swift
@@ -6,26 +6,26 @@ func getData(urlRequest: String) {
         print("Некорректный URL: \(String(describing: urlRequest))")
         return
     }
-
+    
     URLSession.shared.dataTask(with: url) { data, response, error in
         // Ошибка, если имеется
         if let error = error {
             print("Ошибка сетевого запроса: \(error.localizedDescription)")
             return
         }
-
+        
         // Код ответа от сервера
         else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             print("Код ответа от сервера: \(response.statusCode)")
-
-                guard let data = data else {
-                    print("Данные не получены.")
-                    return
-                }
-        // Данные, пришедшие с сервера
-                if let dataAsString = String(data: data, encoding: .utf8) {
-                    print("Данные от сервера: \(dataAsString)")
-                }
+            
+            guard let data = data else {
+                print("Данные не получены.")
+                return
+            }
+            // Данные, пришедшие с сервера
+            if let dataAsString = String(data: data, encoding: .utf8) {
+                print("Данные от сервера: \(dataAsString)")
+            }
         }
     }.resume()
 }
